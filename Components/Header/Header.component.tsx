3be9608ce24470a762dfaede3178c18f071c254d
vip <vip@DESKTOP-TVLU5IH>
2021-10-07 13:13:36 +0200

    optimize font size for small screen

--- Components/Header/Header.component.tsx
+++ Components/Header/Header.component.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './Header.module.css'
-import Image from 'next/image'
 const Header=()=>{
     return(
         <div className={styles.header_wrapper}>      
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css'
 import '../styles/media.css'
+import Head from 'next/head'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+  <Head>
+   <meta name="description" content="Architect website made by next js" />
+  </Head>
+  <Component {...pageProps} />
+  </>
+  )
 }
 export default MyApp
